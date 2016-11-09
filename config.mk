1a307da3932d02587c1f3237c8b6f4b561261a1c
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2016-11-09 18:51:46 +0000

    add -lrt on linux builds

--- config.mk
+++ config.mk
@@ -11,3 +11,10 @@ RANLIB = ranlib
 CPPFLAGS = -D_XOPEN_SOURCE=700
 CFLAGS   = -O2 -std=c99 -Wall -pedantic
 LDFLAGS  = -s
+
+ifneq ($(OS),Windows_NT)
+    UNAME_S := $(shell uname -s)
+    ifeq ($(UNAME_S),Linux)
+        LDFLAGS += -lrt
+    endif
+endif
